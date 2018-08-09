55de59e38c2454147cd3a250e8f0e7916058f719
pentamint <web@pentamint.com>
2018-08-09 14:14:04 +0900

    changed home design etc

--- woocommerce add excerpt to product loop.php
+++
@@ -1,5 +0,0 @@
-add_action( 'woocommerce_after_shop_loop_item_title', 'output_product_excerpt', 20 );
-    function output_product_excerpt() {
-    global $post;
-    echo '<div class="my-excerpt">'.wp_trim_words($post->post_excerpt,10).'</div>';
-}
