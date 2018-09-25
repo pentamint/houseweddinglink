7adf0b01d7e88007d7631480032c811d73fcb6e9
Michael Park <mpark@pentamint.com>
2018-09-25 23:12:40 +0900

    added other sns buttons

--- togglebtn.js
+++ togglebtn.js
@@ -1,7 +1,11 @@
 // ELement toggle add class
-window.onload=function(){
+window.onload = function() {
 document.querySelectorAll('.cosmosfarm-share-button-title')[0].addEventListener('click', function() {
     document.querySelectorAll('.cosmosfarm-naver')[0].classList.toggle('showbtn');
     document.querySelectorAll('.cosmosfarm-facebook')[0].classList.toggle('showbtn');
+    document.querySelectorAll('.cosmosfarm-kakaotalk')[0].classList.toggle('showbtn');
+    document.querySelectorAll('.cosmosfarm-band')[0].classList.toggle('showbtn');
+    document.querySelectorAll('.cosmosfarm-kakaostory')[0].classList.toggle('showbtn');
+    document.querySelectorAll('.cosmosfarm-line')[0].classList.toggle('showbtn');
 }, false);
 }
