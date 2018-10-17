99fd9289de14009ef2f5ade1fa053c490542894f
pentamint <web@pentamint.com>
2018-10-17 18:42:41 +0900

    moved sidebar to top of page, fixed layout bugs

--- searchbtnhide.js
+++
@@ -1,6 +0,0 @@
-// Search button toggle add class
-document.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('.mobile-header-inner .right-btn').addEventListener('click', function() {
-    document.querySelectorAll('.astm-search-menu-wrapper')[0].classList.toggle('hidebtn');
-  }, false);
-});
